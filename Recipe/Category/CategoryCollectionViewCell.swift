cb20c353458f6f1de56f26683846e427ed33c5d9
전보라 <bora@tripstore.kr>
2020-07-12 20:01:37 +0900

    fitst commit

---
+++ Recipe/Category/CategoryCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryCollectionViewCell.swift
+//  Recipe
+//
+//  Created by Bora on 2020/07/05.
+//  Copyright © 2020 Bora. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+}
--- Recipe/ViewController.swift
+++ Recipe/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+}
+
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Category", for: indexPath) as! CategoryCollectionViewCell
+        
+        return cell
+    }
+    
+}
+
+extension ViewController: UICollectionViewDelegate {
+    
 }
 
